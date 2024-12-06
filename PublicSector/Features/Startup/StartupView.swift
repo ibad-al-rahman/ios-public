42e8837a5b1e0a2282a745a366ce37acc46d63ef
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-12-07 00:18:26 +0200

    feat: created startup screen and feature pair

---
+++ PublicSector/Features/Startup/StartupView.swift
@@ -0,0 +1,38 @@
+//
+//  StartupView.swift
+//  PublicSector
+//
+//  Created by Hamza Jadid on 07/12/2024.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct StartupView: View {
+    let store: StoreOf<StartupFeature>
+
+    var body: some View {
+        ZStack {
+            Image("Logo")
+                .resizable()
+                .frame(maxWidth: 1080, maxHeight: 1080)
+                .scaledToFit()
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    StartupView(store: Store(
+        initialState: StartupFeature.State(),
+        reducer: StartupFeature.init
+    ))
+}
+
+#Preview {
+    StartupView(store: Store(
+        initialState: StartupFeature.State(),
+        reducer: StartupFeature.init
+    ))
+    .arabicEnvironment()
+}
