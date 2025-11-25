895e87f5582e0652cbc2a369a9a81ffb4d3ed543
Ghamza-Jd <hamzah.jadeed@gmail.com>
2025-11-25 19:07:08 +0200

    feat: added rocket sim network tool config

--- PublicSector/PublicSectorApp.swift
+++ PublicSector/PublicSectorApp.swift
@@ -11,9 +11,24 @@ import SwiftUI
 struct PublicSectorApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
+    init() {
+        loadRocketSimConnect()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+private func loadRocketSimConnect() {
+    #if DEBUG
+    guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+        print("Failed to load linker framework")
+        return
+    }
+    print("RocketSim Connect successfully linked")
+    #endif
+}
+
