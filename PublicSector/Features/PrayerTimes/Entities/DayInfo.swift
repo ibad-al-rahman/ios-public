8561c70efb6f36df5c6fd26eb31db8fc5a337caf
Ghamza-Jd <hamzah.jadeed@gmail.com>
2026-03-11 05:08:00 +0200

    feat: moved the week prayer times to miqat

--- PublicSector/Features/PrayerTimes/Entities/DayInfo.swift
+++ PublicSector/Features/PrayerTimes/Entities/DayInfo.swift
@@ -6,8 +6,10 @@
 //
 
 import Foundation
+import MiqatKit
 
-struct DayInfo: Equatable {
+struct DayInfo: Equatable, Identifiable {
+    var id: String
     var imsak: Date?
     var fajr: Date
     var sunrise: Date
@@ -17,4 +19,23 @@ struct DayInfo: Equatable {
     var ishaa: Date
 
     var hijri: String
+}
+
+extension DayInfo {
+    init(from miqatData: MiqatData) {
+        self.id = miqatData.id
+        self.imsak = miqatData.imsak
+        self.fajr = miqatData.fajr
+        self.sunrise = miqatData.sunrise
+        self.dhuhr = miqatData.dhuhr
+        self.asr = miqatData.asr
+        self.maghrib = miqatData.maghrib
+        self.ishaa = miqatData.ishaa
+
+        if let localeMonthName = miqatData.hijriLocaleMonth {
+            self.hijri = "\(miqatData.hijriDay) \(localeMonthName) \(miqatData.hijriYear)"
+        } else {
+            self.hijri = ""
+        }
+    }
 }
