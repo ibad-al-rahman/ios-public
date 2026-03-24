79acacc8d56c416eeaeff634751768edc389b953
Ghamza-Jd <hamzah.jadeed@gmail.com>
2026-03-24 02:16:33 +0200

    feat: added background task to schedule notifications

--- PublicSector/AppDelegate.swift
+++ PublicSector/AppDelegate.swift
@@ -5,19 +5,22 @@
 //  Created by Hamza Jadid on 21/01/2025.
 //
 
+import Dependencies
 import FirebaseCore
 import UIKit
 import UserNotifications
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-  ) -> Bool {
-    FirebaseCore.FirebaseApp.configure()
-    UNUserNotificationCenter.current().delegate = self
-    return true
-  }
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseCore.FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
+        @Dependency(\.prayerTimesNotificationScheduler) var scheduler
+        scheduler.registerBackgroundTask()
+        return true
+    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
